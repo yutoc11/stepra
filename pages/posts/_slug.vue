2c55ea08522cbe286d84b81a631d8e366a2a0a18
yutoc11 <kikuchi-yuto@sagojo.link>
2019-10-13 23:52:08 +0900

    記事ページにコラム・カテゴリの表示

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -3,10 +3,14 @@
     .content-wrapper
       .thumb
         img(:src="post.fields.image.fields.file.url")
+      .post-category(v-if="post.fields.category != undefined") {{ post.fields.category.fields.name }}
       h1.post-title {{ post.fields.title }}
       p.post-created-at {{ formatDate(post.sys.createdAt) }}
       .post-description(v-html="$md.render(post.fields.description)")
       .post-content(v-html="$md.render(post.fields.content)")
+      ul.post-tags(v-if="post.fields.tags != undefined")
+        li.tag-list(v-for="tag in post.fields.tags") #
+          | {{ tag.fields.name }}
 </template>
 
 <script>
@@ -56,96 +60,134 @@ methods: {
 
 <style lang="scss">
 article.article {
-.content-wrapper {
-  max-width: 620px;
-  margin: 0 auto;
-  padding:50px 0;
-  .thumb{
-    margin: 30px auto;
-    width: 100%;
-    img{
-      width:100%;
+  .content-wrapper {
+    max-width: 620px;
+    margin: 0 auto;
+    padding:50px 0;
+    .thumb{
+      margin: 18px auto;
+      width: 100%;
+      img{
+        width:100%;
+      }
     }
-  }
-  h2, h3 {
-    margin: 16px 0;
-  }
-  h1.post-title {
-    font-size: 32px;
-    text-decoration: underline;
-    margin: 72px 0 18px;
-    text-decoration: none;
-    line-height:1.5;
-  }
-  .table-of-contents {
-    color: #888C8B;
-    background-color: #F7F9F9;
-    padding: 14px;
-    margin: 36px 0;
-    width: 100%;
-    ul {
-      padding-left:2px;
-      list-style: none;
+    h2, h3 {
+      margin: 16px 0;
     }
-    li {
-      font-size:14px;
-      padding:8px 0;
-      border-bottom: 1px solid #E6E6E6;
+    h1.post-title {
+      font-size: 32px;
+      text-decoration: underline;
+      margin: 48px 0 18px;
+      text-decoration: none;
+      line-height:1.5;
     }
-    li:first-child{
-      padding:20px 0 8px 0;
+    .table-of-contents {
+      color: #888C8B;
+      background-color: #F7F9F9;
+      padding: 14px;
+      margin: 36px 0;
+      width: 100%;
+      ul {
+        padding-left:2px;
+        list-style: none;
+      }
+      li {
+        font-size:14px;
+        padding:8px 0;
+        border-bottom: 1px solid #E6E6E6;
+      }
+      li:first-child{
+        padding:20px 0 8px 0;
+      }
+      li:last-child{
+        border-bottom: none;
+      }
+      &:before {
+        content: '▼ 目次';
+        font-weight: bold;
+        font-size: 14px;
+      }
+      a{
+        color: #888C8B;
+        text-decoration:none;
+      }
+      li:hover,a:hover{
+        color: #000;
+      }
     }
-    li:last-child{
-      border-bottom: none;
+    .post-created-at{
+      color: #D1D3D6;
+      font-size: 12px;
     }
-    &:before {
-      content: '▼ 目次';
+    .post-description{
+      background-color: #F7F9F9;
+      padding: 25px 36px;
+      margin: 36px 0;
+      line-height: 36px;
+      font-size: 16px;
+    }
+    .post-content {
+      h1 {
+        font-size: 32px;
+      }
+      h2 {
+          font-size: 24px;
+          margin:50px 0 -18px;
+        }
+      p {
+        margin: 36px 0;
+        font-size: 16px;
+      }
+      img {
+        max-width: 100%;
+        border: 1px solid #000;
+      }
+      padding: 0 0 15px;
+      position: relative;
+      z-index: 1;
+      > * {
+        position: relative;
+        z-index: 10; // h2のpadding部分より前面に来るように
+      }
+    }
+
+    .post-category{
+      text-align: center;
+      padding: 4px;
+      margin: 0 auto;
+      background-color: #99E1D7;
+      border-radius: 5px;
+      color: #fff;
       font-weight: bold;
       font-size: 14px;
     }
-    a{
-      color: #888C8B;
-      text-decoration:none;
+
+    .post-tags{
+      padding: 0;
     }
-    li:hover,a:hover{
-      color: #000;
+
+    .post-tags{
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
     }
-  }
-  .post-created-at{
-    color: #D1D3D6;
-    font-size: 12px;
-  }
-  .post-description{
-    background-color: #F7F9F9;
-    padding: 25px 36px;
-    margin: 36px 0;
-    line-height: 36px;
-    font-size: 16px;
-  }
-  .post-content {
-    h1 {
-      font-size: 32px;
-    }
-    h2 {
-        font-size: 24px;
-        margin:50px 0 -18px;
-      }
-    p {
-      margin: 36px 0;
-      font-size: 16px;
-    }
-    img {
+    .tag-list{
+      font-size: 14px;
+      display: inline-flex;
+      padding: 4px 10px;
+      margin-right: 5px;
+      border: 1px solid #e6e6e6;
+      background: #fff;
+      border-radius: 4px;
+      cursor: pointer;
+      white-space: nowrap;
+      text-overflow: ellipsis;
       max-width: 100%;
-      border: 1px solid #000;
+      align-items: center;
     }
-    padding: 0 0 15px;
-    position: relative;
-    z-index: 1;
-    > * {
-      position: relative;
-      z-index: 10; // h2のpadding部分より前面に来るように
+    .tag-list:hover{
+      border: 1px solid #A7AAAA;
     }
   }
-}
 }
 </style>
